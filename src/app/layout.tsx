a23d0cb007e0d808dc2b9d045a97bf3f594c6362
Nasrul676 <nasrulmuhammad676@gmail.com>
2025-03-05 10:28:40 +0700

    add google analytic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Registrasi language Center",
@@ -13,8 +14,34 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          id="google-ads"
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'AW-10992316400');`,
+          }}
+        />
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=AW-10992316400"></Script>
+      </head>
       <body>
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PL8Q9N7" height="0" width="0" style={{ display: "none", visibility: "hidden" }}></iframe>
+        </noscript>
         {children}
+        <Script
+          id="google-tag-manager"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-PL8Q9N7');`,
+          }}
+        />
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -161,7 +161,7 @@ export default function Page() {
         </form>
       </CustomLayout>
       <Script
-        id="gtm-script"
+        id="pixel-meta-initialCheckout"
         dangerouslySetInnerHTML={{
           __html: `!(function (f, b, e, v, n, t, s) {
             if (f.fbq) return;
@@ -188,6 +188,61 @@ export default function Page() {
           );
           fbq("init", "1881998885434766");
           fbq("track", "initiateCheckout");`,
+        }}
+      />
+      <Script
+        id="tiktok-pixel"
+        dangerouslySetInnerHTML={{
+          __html: `!(function (w, d, t) {
+            w.TiktokAnalyticsObject = t;
+            var ttq = (w[t] = w[t] || []);
+            (ttq.methods = [
+              "page",
+              "track",
+              "identify",
+              "instances",
+              "debug",
+              "on",
+              "off",
+              "once",
+              "ready",
+              "alias",
+              "group",
+              "enableCookie",
+              "disableCookie",
+            ]),
+              (ttq.setAndDefer = function (t, e) {
+                t[e] = function () {
+                  t.push([e].concat(Array.prototype.slice.call(arguments, 0)));
+                };
+              });
+            for (var i = 0; i < ttq.methods.length; i++)
+              ttq.setAndDefer(ttq, ttq.methods[i]);
+            (ttq.instance = function (t) {
+              for (var e = ttq._i[t] || [], n = 0; n < ttq.methods.length; n++)
+                ttq.setAndDefer(e, ttq.methods[n]);
+              return e;
+            }),
+              (ttq.load = function (e, n) {
+                var i = "https://analytics.tiktok.com/i18n/pixel/events.js";
+                (ttq._i = ttq._i || {}),
+                  (ttq._i[e] = []),
+                  (ttq._i[e]._u = i),
+                  (ttq._t = ttq._t || {}),
+                  (ttq._t[e] = +new Date()),
+                  (ttq._o = ttq._o || {}),
+                  (ttq._o[e] = n || {});
+                n = document.createElement("script");
+                (n.type = "text/javascript"),
+                  (n.async = !0),
+                  (n.src = i + "?sdkid=" + e + "&lib=" + t);
+                e = document.getElementsByTagName("script")[0];
+                e.parentNode.insertBefore(n, e);
+              });
+
+            ttq.load("C36MVV3521OGTSUK8B5G");
+            ttq.page();
+          })(window, document, "ttq");`,
         }}
       />
     </>

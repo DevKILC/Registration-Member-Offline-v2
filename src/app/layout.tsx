82fd11c6a1c26686b09b4b9e21653bf6b7d38365
Nasrul676 <nasrulmuhammad676@gmail.com>
2025-03-11 09:23:36 +0700

    add microsoft clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,16 @@ export default function RootLayout({
           }}
         />
         <Script async src="https://www.googletagmanager.com/gtag/js?id=AW-10992316400"></Script>
-
+        <Script
+          id="microsoft-clarity"
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "qmbw8lb6h7");`,
+          }}
+        />
         <Script
           id="tiktok-pixel"
           dangerouslySetInnerHTML={{

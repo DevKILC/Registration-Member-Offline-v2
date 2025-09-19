9bc8074d0b24308345a8e6af3b1a804b194bc485
RyoSkyKnight <koromoro286@gmail.com>
2025-09-19 13:49:45 +0700

    feat: add useGetParamsDataStore for managing URL parameters and update content_id in useConfirmationPageHooks

--- src/app/hooks/useConfirmationPageHook.ts
+++ src/app/hooks/useConfirmationPageHook.ts
@@ -14,6 +14,7 @@ import { addPaymentInfo as addPaymentInfoMetaPixel } from "./useMetaPixelEvent";
 import { trackAddPaymentInfo as addPaymentInfoTiktokPixel } from "./useTiktokEvent";
 import { useQueryParamsDataStore } from "@/app/hooks/useQueryParamsDataStore";
 import { useCourseDataStore } from "./useCourseDataStore";
+import { useGetParamsDataStore } from "./useGetParamsDataStore";
 
 export const useConfirmationPageHooks = () => {
 
@@ -31,6 +32,7 @@ export const useConfirmationPageHooks = () => {
   const [debouncedValue] = useDebounce(voucher, 200);
   const adminFee = process.env.NEXT_PUBLIC_ADMIN_FEE || 0;
   const { selectedCourse } = useCourseDataStore();
+  const { getParamValue } = useGetParamsDataStore();
 
   // Handle submit form
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
@@ -68,7 +70,7 @@ export const useConfirmationPageHooks = () => {
         value: Number(formData.pembayaran), // Total pembayaran dari form data
         currency: 'IDR',
         content_type: selectedCourse?.name || 'Unknown',
-        content_id: formData.paket?.toString(), // Course package ID
+        content_id: getParamValue("utm_content") || 'Unknown', 
       });
 
       return;
